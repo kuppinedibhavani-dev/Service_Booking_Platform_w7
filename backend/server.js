a701dba03ca9e144db0cbe4e1394cc4b403ce345
kuppinedibhavani_dev <kuppinedibhavani@gmail.com>
2026-06-26 13:20:25 +0530

    solved cors issue

--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-const bookingRoutes = require("./routes/bookingRoutes");
-const adminRoutes = require("./routes/adminRoutes");
-
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
-const cors = require("cors");
-
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/services", require("./routes/serviceRoutes"));
 app.use("/api/bookings", require("./routes/bookingRoutes"));
@@ -23,7 +20,7 @@ app.use("/api/payments", require("./routes/paymentRoutes"));
 app.use("/api/notifications", require("./routes/notificationRoutes"));
 app.use("/api/admin", require("./routes/adminRoutes"));
 
-
+// Test route
 app.get("/", (req, res) => {
   res.send("Service Booking API Running");
 });
